29e547f4fa49250c88c98c30a3332e567ba81f50
Yashash Mallesh <yashumallesh2022@gmail.com>
2025-05-19 18:48:35 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-import xml.etree.ElementTree as Et
+import xml.etree.ElementTree as ET
 RSS_FEED_URL = "http://www.hindustantimes.com/rss/topnews/rssfeed.xml" 
 def loadRSS():
   '''
